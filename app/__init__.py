03da7598cd12b967b8b08624d60f106286e61182
Yuri Istomin <yura.ist@icloud.com>
2017-09-03 21:55:01 +0300

    Add enviroment variables

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 from app import views
--- run.py
+++ run.py
@@ -1,5 +1,7 @@
+import os
 from app import app
 
 
 if __name__ == '__main__':
+    print(os.environ['APP_SETTINGS'])
     app.run()
